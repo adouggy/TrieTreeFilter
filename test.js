c3fb3f8ee97f6f85d40561bb564cd173c38f8977
ade <adouggy@gmail.com>
2015-07-21 14:42:05 +0800

    add Chinese split facade module

---
+++ test.js
@@ -0,0 +1,10 @@
+/**
+ * test here
+ * @author Ade.Li <251089564@qq.com>
+ */
+
+var splitter = require('./split.js');
+
+var arr = splitter.split( '中华人民共和国万岁，万岁，万万岁;法轮功无疆，无疆，无江江' )
+
+console.log( arr );
