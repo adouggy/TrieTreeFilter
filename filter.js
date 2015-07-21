a03f68e3eb8dcfb3eddcb5aa33c4217bd6870bf4
ade <adouggy@gmail.com>
2015-07-21 14:56:11 +0800

    work out

---
+++ filter.js
@@ -0,0 +1,38 @@
+/**
+ * filter, need trie and split module
+ * @author Ade.Li <251089564@qq.com>
+ */
+
+var splitter = require('./split.js');
+var trie = require('./mytrie.js');
+var root = new trie.Node();
+
+/**
+ * input an array of filter words and construct tree
+ */
+function init(filterWords){
+	for( var i =0; i<filterWords.length; i++ ){
+		root.add( filterWords[i] );
+	}
+}
+
+/**
+ * split the post and find whether it contains filter words
+ * return [] if the post pass the filter test
+ * return [fitler words] if post failed, and the result set is the reason
+ */
+function filter(post){
+	var res = [];
+	var arr = splitter.split( post );
+	for( var i=0; i<arr.length; i++ ){
+		if( root.contains( arr[i] ) ){
+			res.push( arr[i] );
+			break;
+		}
+	}
+
+	return res;
+}
+
+module.exports.init = init;
+module.exports.filter = filter;
--- test.js
+++ test.js
@@ -1,27 +1,11 @@
+
+
 /**
  * test here
  * @author Ade.Li <251089564@qq.com>
  */
 
-var splitter = require('./split.js');
-var trie = require('./mytrie.js');
+var filter = require('./filter.js');
 
-var arr = splitter.split( '中华人民共和国万岁，万岁，万万岁;法轮功无疆，无疆，无江江' )
-
-console.log( arr );
-
-
-var root = new trie.Node();
-root.add("abc");
-root.add("abd");
-root.add("cde");
-root.add("你大爷的");
-
-console.log( "test:" + root.contains("abe") );
-console.log( "test:" + root.contains("abc") );
-console.log( "test:" + root.contains("ab") );
-console.log( "test:" + root.contains("abcd") );
-console.log( "test:" + root.contains("abcee") );
-console.log( "test:" + root.contains("cde") );
-console.log( "test:" + root.contains("你大爷的") );
-console.log( "test:" + root.contains("你大爷") );
+filter.init( ['法轮功', '傻逼'] );
+console.log( filter.filter( '中华人民共和国万岁，万岁，万万岁;法轮功无疆，无疆，无江江' ) );
